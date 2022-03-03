836753f16b90d9fb52d26ce6855d2acfb1fdc05a
JinRhim <jinchul.edu@gmail.com>
2022-03-03 14:38:26 -0800

    fourth commit

---
+++ Final_Project/Objects/final_project.d
@@ -0,0 +1,6 @@
+.\objects\final_project.o: Final_Project.c
+.\objects\final_project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\final_project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\final_project.o: ..\..\..\..\inc/tm4c123gh6pm.h
+.\objects\final_project.o: ..\..\..\..\driverlib/sysctl.h
+.\objects\final_project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
---
+++ Final_Project/Objects/startup_rvmdk.d
@@ -0,0 +1,1 @@
+.\objects\startup_rvmdk.o: ..\my_switch\startup_rvmdk.S
